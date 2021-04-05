ee833809d7332b4f52b5dee2897f3e4c8699a488
anaribeiros <anaribeiro3105@gmail.com>
2021-04-05 14:04:13 -0300

    criando cabeçalho (Header)

---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import "../assets/home.css";
+
+function Header(){
+    const Scroll = (amountToScroll) => {
+        window.scroll(0, amountToScroll);
+    }
+    return(
+        <div className="Header fixed-top" >
+            <nav className="navbar navbar-expand-md">
+                <h2 className="mb-0"><b>TECS Hub</b></h2>
+                <div className="collapse navbar-collapse justify-content-around" id="navbarSupportedContent">
+                    <ul className="nav navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Quem somos nós</a>
+                        </li>
+                    </ul>
+                    <ul className="nav navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" onClick={ () => Scroll(400)}>Projetos</a>
+                        </li>
+                    </ul>
+                    <ul className="nav navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" onClick={ () => Scroll(1140)}>Equipe</a>
+                        </li>
+                    </ul>
+                    <ul className="nav navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contato">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
